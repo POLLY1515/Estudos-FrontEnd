0b3d9c83093979f6973ec04479760b5624c86f2a
POLLY1515 <amarantepolly2030@gmail.com>
2023-01-25 00:11:11 -0300

    Aprendendo sobre objetos em javascript

---
+++ Objeto/Aula1.js
@@ -0,0 +1,10 @@
+//criando objeto
+const pessoa = {
+    nome:"poliana",
+    dataNascimento: "1992-07-23",
+    cidade:"conselheiro lafaiete",
+    email:"poliana@gmal.com"
+};
+
+console.log(pessoa.nome);
+console.log(pessoa);
