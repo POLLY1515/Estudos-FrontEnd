2e3ac114c5d10562e0bd5bb219e0cf916635a4e8
POLLY1515 <amarantepolly2030@gmail.com>
2023-05-09 00:58:41 -0300

    Trabalhando com operações aritméticas

---
+++ Operacao-Aritmetica/script.js
@@ -0,0 +1,5 @@
+console.log(`Adição: 10 + 5 = ${10 + 5}`);
+console.log(`Subtração 4 - 3 = ${4 - 3}`);
+console.log(`Multiplicação 8 * 9 = ${8 * 9}`);
+console.log(`Divisão 20 / 3 = ${20 / 3}`);
+console.log(`Resto da divisão de 8 por 2 = ${8 % 2}`);
